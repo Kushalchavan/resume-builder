da19ce2d8ee08c115b72188887591be3882bafe7
Kushalchavan <kushalchavan2002@gmail.com>
2025-11-12 14:07:00 +0530

    ✨ feat: add resume apis

--- server/src/config/imagekit.js
+++
@@ -1,6 +0,0 @@
-import ImageKit from '@imagekit/nodejs';
-
-export const imagekit = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
-});
-
--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/user.route.js";
-import resumeRouter from "./routes/resume.route.js";
 import { connectDb } from "./config/db.js";
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/resumes", resumeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
